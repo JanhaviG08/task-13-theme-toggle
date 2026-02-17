191083ba0ec17311eb11afaefc780084a52263fc
JanhaviG08 <144892282+JanhaviG08@users.noreply.github.com>
2026-02-17 19:27:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const themeBtn = document.getElementById("themeBtn");
+
+// Load saved theme
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme) {
+    document.documentElement.setAttribute("data-theme", savedTheme);
+}
+
+// Toggle theme
+themeBtn.addEventListener("click", () => {
+    const currentTheme = document.documentElement.getAttribute("data-theme");
+
+    if (currentTheme === "dark") {
+        document.documentElement.removeAttribute("data-theme");
+        localStorage.setItem("theme", "light");
+    } else {
+        document.documentElement.setAttribute("data-theme", "dark");
+        localStorage.setItem("theme", "dark");
+    }
+});
